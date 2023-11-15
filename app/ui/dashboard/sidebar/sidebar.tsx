d65bf3219e5a9e9c85b27cd6d4c8e980635c7523
Ramanand Thakur <ramanandt35@gmail.com>
2023-11-16 00:45:48 +0530

    structure defined

---
+++ app/ui/dashboard/sidebar/sidebar.tsx
@@ -0,0 +1,11 @@
+import styles from './sidebar.module.css';
+
+const Sidebar = () => {
+  return (
+    <div className={styles.container}>
+      Sidebar
+    </div>
+  )
+}
+
+export default Sidebar
