4f7273158989566b48fdc1d945b710cb441d9ea1
Ramanand Thakur <ramanandt35@gmail.com>
2023-11-17 23:21:40 +0530

    sidebar added

--- app/ui/dashboard/sidebar/sidebar.tsx
+++ app/ui/dashboard/sidebar/sidebar.tsx
@@ -1,11 +1,108 @@
+import Image from 'next/image';
+import MenuLink from './menuLink/menuLink';
 import styles from './sidebar.module.css';
+import {
+  MdDashboard,
+  MdSupervisedUserCircle,
+  MdShoppingBag,
+  MdAttachMoney,
+  MdWork,
+  MdAnalytics,
+  MdPeople,
+  MdOutlineSettings,
+  MdHelpCenter,
+  MdLogout,
+} from 'react-icons/md'
+const menuItems = [
+  {
+    title: 'Pages',
+    list: [
+      {
+        title: 'Dashboard',
+        path: '/dashboard',
+        icon: <MdDashboard />
+      },
+      {
+        title: 'Users',
+        path: '/dashboard/users',
+        icon: <MdSupervisedUserCircle />
+      },
+      {
+        title: 'Products',
+        path: '/dashboard/products',
+        icon: <MdShoppingBag />
+      },
+      {
+        title: 'Transactions',
+        path: '/dashboard/transactions',
+        icon: <MdAttachMoney />
+      },
+    ],
+  },
+  {
+    title: 'Analytics',
+    list: [
+      {
+        title: 'Revenue',
+        path: '/dashboard/revenue',
+        icon: <MdWork />
+      },
+      {
+        title: 'Reports',
+        path: '/dashboard/reports',
+        icon: <MdAnalytics />
+      },
+      {
+        title: 'Teams',
+        path: '/dashboard/teams',
+        icon: <MdPeople />
+      },
+    ],
+  },
+  {
+    title: 'User',
+    list: [
+      {
+        title: 'Settings',
+        path: '/dashboard/settings',
+        icon: <MdOutlineSettings />,
+      },
+      {
+        title: 'Help',
+        path: '/dashboard/help',
+        icon: <MdHelpCenter />,
+      },
+    ],
+  },
+];
 
 const Sidebar = () => {
   return (
     <div className={styles.container}>
-      Sidebar
+      <div className={styles.user}>
+        <Image className={styles.userImage}
+        alt='User image'
+        src="/user.png"
+        width={50}
+        height={50} />
+        <div className={styles.userDetails}>
+          <span className={styles.username}>Tony Stark</span>
+          <span className={styles.userTitle}>Admin</span>
+        </div>
+      </div>
+      
+      <ul>
+        {menuItems.map(category => (
+          <li key={category.title}>
+            <span className={styles.category}>{category.title}</span>
+            {category.list?.map(item =>(
+              <MenuLink item={item} key={item.title} />
+            ))}
+          </li>
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
 export default Sidebar
