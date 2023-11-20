f04ee1170dc8b41697e1f84ef08a0418746e339f
Ramanand Thakur <ramanandt35@gmail.com>
2023-11-20 23:53:53 +0530

    transaction table created

--- app/ui/dashboard/transaction/transaction.tsx
+++ app/ui/dashboard/transaction/transaction.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import Image from 'next/image';
+import styles from './transaction.module.css';
 
 const Transaction = () => {
   return (
-    <div>
-      transaction
+    <div className={styles.container}>
+      <h2 className={styles.title}>Latest Transactions</h2>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <td>Name</td>
+            <td>Status</td>
+            <td>Date</td>
+            <td>Amount</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <Image alt='' src='/user.png' width={40} height={40} className={styles.userImage} />
+              Tony Stark
+            </td>
+            <td>
+              <span className={`${styles.status} ${styles.pending}`}>Pending</span>
+            </td>
+            <td>11/11/1999</td>
+            <td>INR. 10000</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   )
 }
