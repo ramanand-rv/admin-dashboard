63bfb1b49625e5d8253317d7f9bed756559a09bc
Ramanand Thakur <ramanandt35@gmail.com>
2023-11-20 23:40:27 +0530

    card component styled and added

--- app/ui/dashboard/card/card.tsx
+++ app/ui/dashboard/card/card.tsx
@@ -1,11 +1,17 @@
+import { MdSupervisedUserCircle } from 'react-icons/md';
 import styles from './card.module.css';
 
-const Card = () => {
-  return (
-    <div>
-      
+const Card = () => (
+  <div className={styles.container}>
+    <MdSupervisedUserCircle size={24} />
+    <div className={styles.texts}>
+      <span className={styles.title}>Total Users</span>
+      <span className={styles.number}> {Math.floor(Math.random()*100)}</span>
+      <span className={styles.detail}>
+        <span className={styles.positive}>12%</span> more than previous week
+      </span>
     </div>
-  )
-}
+  </div>
+)
 
 export default Card
