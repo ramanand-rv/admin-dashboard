a1db4b050e08b9f40551c3b18cdcbd323e189b92
Ramanand Thakur <ramanandt35@gmail.com>
2023-11-22 00:02:13 +0530

    chart added

--- app/ui/dashboard/chart/chart.tsx
+++ app/ui/dashboard/chart/chart.tsx
@@ -43,6 +43,7 @@ const data = [
 const Chart = () => {
   return (
     <div className={styles.container}>
+      <h2 className={styles.title}>Weekly Recap</h2>
       <ResponsiveContainer width="100%" height="100%">
         <LineChart
           width={500}
@@ -60,8 +61,8 @@ const Chart = () => {
           <YAxis />
           <Tooltip />
           <Legend />
-          <Line type="monotone" dataKey="click" stroke="#8884d8" strokeDasharray="5 5" />
-          <Line type="monotone" dataKey="visit" stroke="#82ca9d" strokeDasharray="3 4 5 2" />
+          <Line type="monotone" dataKey="visit" stroke="#8884d8" strokeDasharray="5 5" />
+          <Line type="monotone" dataKey="click" stroke="#82ca9d" strokeDasharray="3 4 5 2" />
         </LineChart>
       </ResponsiveContainer>
     </div>
