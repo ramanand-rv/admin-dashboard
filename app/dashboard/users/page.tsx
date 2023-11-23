bd04316fee5ad1f5e80878ddb4b12fc65b4b0682
Ramanand Thakur <ramanandt35@gmail.com>
2023-11-23 07:19:51 +0530

    user table added

--- app/dashboard/users/page.tsx
+++ app/dashboard/users/page.tsx
@@ -1,5 +1,6 @@
 import Search from '@/app/ui/dashboard/search/search';
 import styles from '@/app/ui/dashboard/users/users.module.css';
+import Image from 'next/image';
 import Link from 'next/link';
 
 function UsersPage() {
@@ -12,7 +13,34 @@ function UsersPage() {
         </Link>
       </div>
       <table className={styles.table}>
-
+        <thead>
+          <tr>
+            <td>Name</td>
+            <td>Email</td>
+            <td>Created at</td>
+            <td>Role</td>
+            <td>Status</td>
+            <td>Action</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td className={styles.user}>
+              <Image src='/user.png' className={styles.userImage} alt='user image' width={40} height={40}/>
+              Tony stark
+            </td>
+            <td>tony_stark@gmail.com</td>
+            <td>11.11.2023</td>
+            <td>CEO</td>
+            <td>Active</td>
+            <td>
+              <Link href='/'>
+                <button className={`${styles.button} ${styles.view}`}>View</button>
+              </Link>
+              <button className={`${styles.button} ${styles.view}`}>Delete</button>
+            </td>
+          </tr>
+        </tbody>
       </table>
     </div>
   )
