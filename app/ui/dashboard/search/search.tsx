15d58554fbe993373a931af1760e9393aa1662dd
Ramanand Thakur <ramanandt35@gmail.com>
2023-11-29 07:25:16 +0530

    path query based search designed

--- app/ui/dashboard/search/search.tsx
+++ app/ui/dashboard/search/search.tsx
@@ -1,13 +1,29 @@
+'use client'
 import { MdSearch } from 'react-icons/md';
 import styles from './search.module.css';
+import { usePathname, useSearchParams, useRouter } from 'next/navigation';
 
 const Search = ({placeholder}: any) => {
+  const searchParams = useSearchParams();
+  const pathName = usePathname();
+  const {replace} = useRouter();
+  const params = new URLSearchParams(searchParams);
+  params.set('test', 'value')
+  replace(`${pathName}?${params}`);
+
+  console.log(searchParams);
+  console.log(pathName);
+
+  const handleSearch = (e) =>{
+    
+  }
   return (
     <div className={styles.container}>
       <MdSearch />
       <input type="text" 
       placeholder={placeholder} 
       className={styles.input}
+      onChange={handleSearch}
       />
     </div>
   )
