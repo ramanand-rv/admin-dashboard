30fe2625139fe9685231e918b84de0eedc50e796
Ramanand Thakur <ramanandt35@gmail.com>
2023-11-30 16:01:05 +0530

    added search product and fixed pagination

--- app/dashboard/products/page.tsx
+++ app/dashboard/products/page.tsx
@@ -31,19 +31,19 @@ const ProductsPage = async ({ searchParams }: any) => {
           </tr>
         </thead>
         <tbody>
-          {products.map((product) => (
+          {products.map((product:any) => (
             <tr key={product.id}>
               <td className={styles.product}>
                 <Image src={product.img || '/noproduct.png'} className={styles.productImage} alt='product image' width={40} height={40} />
                 {product.title}
               </td>
-              <td>{product.description}</td>
+              <td>{product.description.toString().slice(0, 10)}</td>
               <td>INR {product.price}</td>
               <td>{product.createdAt?.toString().slice(4, 16)}</td>
               <td>{product.stock}</td>
               <td>
                 <div className={styles.buttons}>
-                  <Link href='/dashboard/products/test'>
+                  <Link href={`/dashboard/products/${product.id}`}>
                     <button className={`${styles.button} ${styles.view}`}>View</button>
                   </Link>
                   <button className={`${styles.button} ${styles.delete}`}>Delete</button>
@@ -53,7 +53,7 @@ const ProductsPage = async ({ searchParams }: any) => {
           ))}
         </tbody>
       </table>
-      <Pagination />
+      <Pagination count={count} />
     </div>
   )
 }
