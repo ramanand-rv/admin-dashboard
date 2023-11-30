d3a2d1734a11b555edc446dbb144fd426882fb10
Ramanand Thakur <ramanandt35@gmail.com>
2023-11-30 15:13:39 +0530

    fetch product method added

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,18 +1,32 @@
-import { User } from "./models";
+import { User, Product } from "./models";
 import { connectToDB } from "./utils";
 
 export const fetchUsers = async (q: any, page: any) => {
-
     const regex = new RegExp(q, 'i');
     const UsersPerPage = 5;
     try {
         connectToDB();
         const count = (await User.find({ username: { $regex: regex } })).length;
-        const users = await User.find({username: {$regex: regex}}).limit(UsersPerPage).skip(UsersPerPage * (page - 1)); 
-        
-        return {count, users};
+        const users = await User.find({ username: { $regex: regex } }).limit(UsersPerPage).skip(UsersPerPage * (page - 1));
+
+        return { count, users };
     } catch (error) {
         console.log(error);
         throw new Error('Failed to fetch users');
     }
-}
+};
+
+export const fetchProducts = async (q: any, page: any) => {
+    const regex = new RegExp(q, 'i');
+    const ProductPerPage = 5;
+    try {
+        connectToDB();
+        const count = (await Product.find({ title: { $regex: regex } })).length;
+        const products = await Product.find({ title: { $regex: regex } }).limit(ProductPerPage).skip(ProductPerPage * (page - 1));
+
+        return { count, products };
+    } catch (error) {
+        console.log(error);
+        throw new Error('Failed to fetch Products');
+    }
+};
